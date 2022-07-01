ece50abe70f0bab92920d190535d32bc40e68326
SvetlanaNikonova <SvetlanaNikonova@coherentsolutions.com>
2022-07-01 16:06:48 +0300

    implicitWait added

--- src/test/java/com/coherentsolutions/training/aqa/java/web/nikonova/YandexLoginTest.java
+++ src/test/java/com/coherentsolutions/training/aqa/java/web/nikonova/YandexLoginTest.java
@@ -10,6 +10,8 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class YandexLoginTest {
 
     private WebDriver driver;
@@ -19,31 +21,30 @@ public class YandexLoginTest {
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @Parameters({"userName", "password"})
     @Test
     public void loginTest(String userName, String password) throws InterruptedException {
+
         driver.get(URL);
 
         driver.findElement(By.cssSelector(".HeadBanner-ButtonsWrapper > a:last-child")).click();
 
         String title = driver.getTitle();
-        Thread.sleep(1000);
         Assert.assertEquals("Authorization", title);
 
         WebElement usernameInput = driver.findElement(By.id("passp-field-login"));
         usernameInput.clear();
         usernameInput.sendKeys(userName);
         driver.findElement(By.id("passp:sign-in")).click();
-        Thread.sleep(1000);
         WebElement passwordInput = driver.findElement(By.id("passp-field-passwd"));
         passwordInput.clear();
         passwordInput.sendKeys(password);
         driver.findElement(By.id("passp:sign-in")).click();
 
         String inboxPageTitle = "Inbox";
-        Thread.sleep(2000);
         Assert.assertTrue(driver.getTitle().contains(inboxPageTitle));
     }
 

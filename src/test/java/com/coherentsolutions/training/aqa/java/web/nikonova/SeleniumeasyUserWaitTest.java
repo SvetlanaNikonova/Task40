c5c4e8bbe934303b90d1a03050748d028f39d07f
SvetlanaNikonova <SvetlanaNikonova@coherentsolutions.com>
2022-07-04 15:02:46 +0300

    alert box tested

---
+++ src/test/java/com/coherentsolutions/training/aqa/java/web/nikonova/SeleniumeasyUserWaitTest.java
@@ -0,0 +1,43 @@
+package com.coherentsolutions.training.aqa.java.web.nikonova;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class SeleniumeasyUserWaitTest {
+
+    private WebDriver driver;
+    private final String URL = "https://demo.seleniumeasy.com/dynamic-data-loading-demo.html";
+
+    @BeforeTest
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void waitTest() {
+
+        driver.get(URL);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement button = driver.findElement(By.cssSelector("#save"));
+        button.click();
+        WebElement portrait = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#loading > img[src*='randomuser.me/api/portraits']")));
+        Assert.assertTrue(portrait.isDisplayed());
+    }
+
+    @AfterTest
+    public void cleanUp() {
+        driver.quit();
+    }
+}
